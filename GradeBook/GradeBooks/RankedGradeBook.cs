51eada836729083ec3895fc7ca18e7d2c963bba8
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-03-23 05:48:29 +0100

    override CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,19 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+       
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            } 
+        }
     }
 }
