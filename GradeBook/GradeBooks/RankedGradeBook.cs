b7f2427981f6d22fc752761057be5274736f61bc
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-03-28 18:46:45 +0200

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -50,6 +50,10 @@ namespace GradeBook.GradeBooks
             {
                 return 'D';
             }
+            else
+            {
+                return 'F';
+            }
            
         }   
     }
